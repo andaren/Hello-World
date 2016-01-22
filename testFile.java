d913180bff977984c1b8c1c1f8a4786eb9937fc6
andaren <603822061@qq.com>
2016-01-23 01:06:29 +0800

    a new file
    this is servlet file

---
+++ testFile.java
@@ -0,0 +1,97 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class yazServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public yazServlet() {		
+		super();
+		System.out.println("初始化yazServlet");
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		
+		String d = request.getParameter("d");
+		if (d == null) {
+			System.out.println("123");
+			return;
+		}
+		if (d.equals("servlet")) {
+			request.setAttribute("MSG", "This is from yazServlet MSG!中文");
+			request.getRequestDispatcher("/servlet/anotherServlet").forward(request, response);
+		} else if (d.equals("file")) {
+			request.getRequestDispatcher("/WEB-INF/web.xml").forward(request, response);
+		} else if (d.equals("jsp")) {
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+		} 
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the POST method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
